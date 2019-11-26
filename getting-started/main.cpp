c03c29c227040c7e65fa002ec010847fac80d5b7
Filip Jagodzinski <fkjagodzinski@gmail.com>
2019-11-26 15:37:40 +0100

    Include "mbed.h" to enable GCC_ARM build with "-flto"
    Add the "mbed.h" header to use the MBED_USED attribute with main().
    
    Since the "used" attribute and the definition of main() have to be in
    the same translation unit, I placed a declaration of main() in
    "platform/mbed_toolchain.h" which "mbed.h" includes.
    
    Without the attribute, the main() symbol is not emitted with the GCC
    toolchain using "-Wl,--wrap,main" and "-flto" flags.

--- getting-started/main.cpp
+++ getting-started/main.cpp
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "mbed.h"
 #include "psa/crypto.h"
 #include "mbedtls/version.h"
 #include <string.h>
